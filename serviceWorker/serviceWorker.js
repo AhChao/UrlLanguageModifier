7e1dbc921514d28164a3a630f7ecd85d499a6ca5
stevne.chao <gash612@gmail.com>
2023-07-06 10:41:30 +0800

    [Feat] Implement hot key open in new window

--- serviceWorker/serviceWorker.js
+++ serviceWorker/serviceWorker.js
@@ -1,15 +1,23 @@
 let currentTab = null;
 let targetUrl = "";
 let dir = "fromToTo";
+let shiftPressed = false;
 
 chrome.commands.onCommand.addListener(function (command) {
     console.log('onCommand event received for message: ', command);
     if (command == "transToLan") {
         dir = "fromToTo";
+        shiftPressed = false;
         updateCurrentTabUrlVariable();
     }
     if (command == "transFromLan") {
         dir = "toToFrom";
+        shiftPressed = false;
+        updateCurrentTabUrlVariable();
+    }
+    if (command == "transToLanNew") {
+        dir = "fromToTo";
+        shiftPressed = true;
         updateCurrentTabUrlVariable();
     }
 });
@@ -21,8 +29,12 @@ function updateCurrentTab() {
     if ((currentUrl.indexOf(fromLan) != -1 && dir == "fromToTo") ||
         (currentUrl.indexOf(toLan) != -1 && dir == "toToFrom")) {
         targetUrl = dir == "fromToTo" ? currentUrl.replace(fromLan, toLan) : currentUrl.replace(toLan, fromLan);
-        chrome.tabs.update(
-            currentTab.id, { url: targetUrl }, null);
+        if (shiftPressed) {
+            chrome.tabs.create({ url: targetUrl });
+        }
+        else {
+            chrome.tabs.update(currentTab.id, { url: targetUrl }, null);
+        }
     }
 }
 
